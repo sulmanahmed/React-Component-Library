d3b7fd233eff8bc116ca55dcae16a84e44715456
Sulman Ahmed <sulman.ahmed@theowlsense.com>
2022-12-12 10:17:23 +0500

    local fonts added

--- src/Components/TestComponent/TestComponent.js
+++ src/Components/TestComponent/TestComponent.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import InputField from "../../CommonComponents/InputField/InputField";
 import TextArea from "../../CommonComponents/TextArea/TextArea";
 import PropTypes from "prop-types";
+import "./TestComponent.css"
 
 function TestComponent({handleChange}) {
 
@@ -17,6 +18,7 @@ function TestComponent({handleChange}) {
         />
 
         <TextArea inputClass="form-control m-2" rows="3" handleChange={handleChange} />
+        <div className="App-Section"> hello how are you</div>
       </div>
     </>
   );
